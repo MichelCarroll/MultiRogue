24177365fe1c6e58c32f4c107107c7c3ec48cc35
Michel Carroll <carrmi.0@gmail.com>
2015-10-01 17:38:45 -0400

    done, but weird things going on....

--- src/server/ArtificialClient.ts
+++ src/server/ArtificialClient.ts
@@ -14,6 +14,7 @@ class ArtificialClient {
 
     private viewpoint:Viewpoint;
     private messageClient:MessageClient;
+    private isAloneOnServer = true;
 
     constructor(messageServer:MessageServer) {
         var self = this;
@@ -25,33 +26,46 @@ class ArtificialClient {
 
     private hookOnEvents() {
         var self = this;
+        var connectedPlayers = 0;
 
         this.messageClient.on('sync', function(message:Message) {
             self.viewpoint = (message.getData().viewpoint);
+            self.takeTurnIfApplicable();
+        });
+
+        this.messageClient.on('player-came', function(message:Message) {
+            connectedPlayers++;
+            self.isAloneOnServer = connectedPlayers == 0;
+            self.messageClient.send(new Message('sync-request'));
+        });
+
+        this.messageClient.on('player-left', function(message:Message) {
+            connectedPlayers--;
+            self.isAloneOnServer = connectedPlayers == 0;
+            this.messageClient.send(new Message('sync-request'));
         });
 
         this.messageClient.on('initiate', function(message:Message) {
-            self.viewpoint = (message.getData().viewpoint);
+            self.viewpoint = message.getData().viewpoint;
+            connectedPlayers = message.getData().level.players.length;
+            self.isAloneOnServer = connectedPlayers == 0;
         });
 
         this.messageClient.on('its-your-turn', function(message:Message) {
-            console.log('my turn!');
             self.viewpoint.setPlayer(message.getData().player);
-            var remainingTurns = self.viewpoint.getPlayer().getPlayableComponent().getRemainingTurns();
-            var i = 0;
-            var timeout = setInterval(function() {
-                if(i++ < remainingTurns) {
-                    self.messageClient.send(new Message('idle'));
-                }
-                else {
-                    clearTimeout(timeout);
-                }
-            }, 1000);
+            self.takeTurnIfApplicable();
         });
 
         this.messageClient.send(new Message('ready'));
     }
 
+    private takeTurnIfApplicable() {
+        var remainingTurns = this.viewpoint.getPlayer().getPlayableComponent().getRemainingTurns();
+        if(remainingTurns == 0 || this.isAloneOnServer) {
+            return;
+        }
+        this.messageClient.send(new Message('idle'));
+    }
 
 }
 
--- src/server/main.js
+++ src/server/main.js
@@ -7,4 +7,8 @@ var ServerParameters = require('./lib/ServerParameters');
 //var randomSeed = Date.now();
 var randomSeed = 19582923;
 
-var server = new GameServer({randomSeed: randomSeed, port: 3000});
+var server = new GameServer({
+    randomSeed: randomSeed,
+    port: 3000,
+    includeFakeClient: true
+});
