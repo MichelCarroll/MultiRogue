10c4eed31e91bc9b49064ad7dc118f0afe666351
Michel Carroll <carrmi.0@gmail.com>
2015-10-02 17:03:03 -0400

    cleaned up test

--- test/suits/pathfinding.js
+++ test/suits/pathfinding.js
@@ -5,31 +5,39 @@ var Simulator = require('../common');
 describe('client connects in a level with a follow bot in it', function() {
     var client;
     var server;
+
+    var robotStartsChase = function() {
+        return client.hasInLog("Robot #1051 shouts \"*looks at Player #1052* Fresh meat..\"!!");
+    };
+
+    var robotStopsChase = function() {
+        return client.hasInLog("Robot #1051 shouts \"Hmm... Where did he go..?\"!!");
+    };
+
     beforeEach(function(){
         server = Simulator.serverBoots({includeFollowBot: true});
         client = server.clientConnects();
     });
+
     it('should not be alone', function() {
         should(client.hasInLog("You're now connected as Player #1052!")).be.true();
     });
+    
     it('nothing should happen if player stays where he is', function(done) {
         client.wait().then(function() {
             return client.idle().wait();
         }).then(function() {
-            return client.idle().wait();
-        }).then(function() {
-            return client.idle().wait();
-        }).then(function() {
-            should(client.hasInLog("Robot #1051 shouts \"*looks at Player #1052* Fresh meat..\"!!")).be.false();
-            should(client.hasInLog("Robot #1051 shouts \"Hmm... Where did he go..?\"!!")).be.false();
+            should(robotStartsChase()).be.false();
+            should(robotStopsChase()).be.false();
         }).done(done, done);
     });
+
     it('robot should react if player moves close to him', function(done) {
         client.wait().then(function() {
             return client.moveLeft().moveLeft().moveLeft().moveLeft().wait();
         }).then(function() {
-            should(client.hasInLog("Robot #1051 shouts \"*looks at Player #1052* Fresh meat..\"!!")).be.true();
-            should(client.hasInLog("Robot #1051 shouts \"Hmm... Where did he go..?\"!!")).be.false();
+            should(robotStartsChase()).be.true();
+            should(robotStopsChase()).be.false();
         }).done(done, done);
     });
 });
